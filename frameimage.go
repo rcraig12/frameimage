e7fdbabededad135506a14c901506c01276bd7c6
Robert Craig <rcraig12@gmail.com>
2024-01-04 03:04:08 +0000

    new code

--- frameimage.go
+++ frameimage.go
@@ -1,8 +1,36 @@
 package frameimage
 
-import "fmt"
+import (
+	"image"
+	"log"
+
+	"github.com/disintegration/imaging"
+)
 
 // Render framed image
 func Render(printImage, frameImage string) {
-	fmt.Println("Hello from framed image")
+	    // Load the source image
+			srcImage, err := imaging.Open(printImage)
+			if err != nil {
+					log.Fatalf("failed to open image: %v", err)
+			}
+	
+			// Load the frame image (this is a simplified example)
+			frameImg, err := imaging.Open(frameImage)
+			if err != nil {
+					log.Fatalf("failed to open frame image: %v", err)
+			}
+	
+			// Resize frame to desired dimensions (this is a simplified example)
+			frameResized := imaging.Resize(frameImg, srcImage.Bounds().Dx(), 50, imaging.Lanczos)
+	
+			// Apply the frame to the top of the source image
+			// For a full implementation, you'd need to apply it to all sides and handle corners
+			dstImage := imaging.Overlay(srcImage, frameResized, image.Pt(0, 0), 1.0)
+	
+			// Save the resulting image
+			err = imaging.Save(dstImage, "./framed_image.jpg")
+			if err != nil {
+					log.Fatalf("failed to save image: %v", err)
+			}
 }
